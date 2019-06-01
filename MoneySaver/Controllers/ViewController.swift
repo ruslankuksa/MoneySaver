b2c0853a5318b257bc82fb109fb15d1b80eb0ab2
Ruslan Kuksa <templar1128@hotmail.com>
2019-06-01 07:35:10 +0300

    some bug fixes

--- MoneySaver/Controllers/ViewController.swift
+++ MoneySaver/Controllers/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet var tableView: UITableView!
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    var fetchedResultController: NSFetchedResultsController<Expense>!
+    var fetchedResultController: NSFetchedResultsController<Transaction>!
     
     let defaults = UserDefaults.standard
     var balance: Float = 0
@@ -29,6 +29,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         balance = defaults.float(forKey: "Balance")
         balanceLabel.text = NSString(format: "%.2f", balance) as String
         
+        //loadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         loadData()
     }
     
@@ -54,7 +59,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func configureCell(_ cell: Cell, at indexPath: IndexPath) {
         guard let transaction = self.fetchedResultController?.object(at: indexPath) else {
-            fatalError("Can't configure cell") }
+            fatalError("Can't configure cell")
+        }
         
         cell.categoryLabel.text = transaction.category
         cell.noteLabel.text = transaction.note ?? ""
@@ -94,7 +100,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func newTransaction(amount: Float, category: String, note: String, date: Date, spend: Bool) {
-        let transaction = Expense(context: context)
+        let transaction = Transaction(context: context)
         
         
         transaction.category = category
@@ -102,8 +108,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         transaction.date = date
         transaction.amount = amount
         transaction.expanse = spend
-        
-        //operations.append(transaction)
         
         saveData()
         
@@ -132,15 +136,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func loadData() {
         
-        let fetchRequest: NSFetchRequest<Expense> = Expense.fetchRequest()
+        let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: "date", ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
         
-        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: #keyPath(Expense.isDate), cacheName: nil)
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: #keyPath(Transaction.isDate), cacheName: nil)
         fetchedResultController.delegate = self
         
         do {
-            //operations = try context.fetch(fetchRequest)
             try fetchedResultController.performFetch()
         } catch {
             print("Load data error: \(error)")
@@ -151,7 +154,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 }
 
-extension Expense {
+extension Transaction {
     
     @objc var isDate: String {
         get {
@@ -173,32 +176,52 @@ extension ViewController: NSFetchedResultsControllerDelegate {
     }
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+
         switch type {
         case .insert:
             if let indexPath = newIndexPath {
+                print("Added:\(indexPath)")
                 tableView.insertRows(at: [indexPath], with: .fade)
             }
             break
         case .delete:
             if let indexPath = indexPath {
+                print("Delete:\(indexPath)")
                 tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+        case .update:
+            if let indexPath = indexPath {
+                print("Update:\(indexPath)")
+                let cell = tableView.cellForRow(at: indexPath) as! Cell
+                configureCell(cell, at: indexPath)
             }
             break
         case .move:
             if let indexPath = indexPath {
                 tableView.deleteRows(at: [indexPath], with: .fade)
             }
-            
             if let newIndexPath = newIndexPath {
+                print("Move:\(newIndexPath)")
                 tableView.insertRows(at: [newIndexPath], with: .fade)
             }
             break
-        case .update:
-            if let indexPath = indexPath {
-                let cell = tableView.cellForRow(at: indexPath) as! Cell
-                configureCell(cell, at: indexPath)
-            }
         @unknown default:
+            break
+        }
+        
+        
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+            break
+        case .delete:
+            tableView.deleteSections(NSIndexSet(index: sectionIndex) as IndexSet, with: .fade)
+            break
+        default:
             break
         }
     }
